47076b9515e381b1c9f5ec8746f2615d2b3ef08b
Renan Franca <renansf82@gmail.com>
2024-10-22 15:55:41 -0300

    alterado na 1

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ document.body.innerText = "Hello World!!!"
 
 document.body.innerText = "Texto Modificado"
 
-document.body.innerText = "Teste git upstream"
+document.body.innerText = "Teste git upstream alterado na 1"
 
 document.body.innerText = "Testando merge com push da master"
